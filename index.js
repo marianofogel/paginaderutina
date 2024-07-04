8fe9d40131af60f00b7dcba3a29fe83ed701c20a
Mariano <Marianofogel17@gmail.com>
2024-07-04 15:51:53 -0300

    Se agregaron los index correspondientes a css, js.

---
+++ index.js
@@ -0,0 +1,48 @@
+const botonmarian = document.getElementById('botonmarian')
+const botonmati = document.getElementById('botonmati')
+
+let passwordMarian = "marianito"
+let passwordMati = "matulais"
+let contadorMarian = 0;
+let contadorMati = 0;
+const total = 2;
+
+
+    botonmarian.addEventListener("click", () => {
+
+        if (contadorMarian === 1) {
+            alert('Ya pusiste la contraseña')
+            return;
+        }
+        let clave = prompt("Pone la contraseña Marian.");
+
+        if (passwordMarian === clave) {
+            alert('Contraseña correcta.')
+            contadorMarian++;
+            console.log(contadorMarian)
+
+        } else {
+            alert('Contraseña Incorrecta.')
+        }
+    })
+
+    botonmati.addEventListener("click", () => {
+
+        if (contadorMati === 1) {
+            alert('Ya pusiste la contraseña')
+            return;
+        }
+        let clave = prompt("Pone la contraseña Marian.");
+
+        if (passwordMati === clave) {
+            alert('Contraseña correcta.')
+            contadorMati++;
+            console.log(contadorMati)
+
+        } else {
+            alert('Contraseña Incorrecta.')
+        }
+    })
+    
+
+
